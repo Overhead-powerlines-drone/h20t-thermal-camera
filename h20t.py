937112d7e2c396bcc38f7b4986080aad418c73d2
oryyxx <113963035+oryyxx@users.noreply.github.com>
2022-10-25 08:14:11 +0400

    added combine2Center()

--- h20t.py
+++ h20t.py
@@ -5,6 +5,31 @@ import numpy as np
 videofile = 'demo.MP4'
 cap = cv2.VideoCapture(videofile)
 
+def resize(img, scale = 100):
+    
+    width = int(img.shape[1] * scale / 100)
+    height = int(img.shape[0] * scale / 100)
+    dim = (width, height)
+    
+    # resize image
+    return cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+
+def combine2Center(img1, img2):
+
+    img2 = resize(img2, 139)
+    h1, w1, _ = img1.shape
+    h2, w2, _ = img2.shape
+
+    xoffset = int((w1 / 2) - (w2 / 2))
+    yoffeset = int((h1 / 2) - (h2 / 2) + 12)
+    
+    ret = img1.copy()
+
+    ret[yoffeset:yoffeset + img2.shape[0], xoffset:xoffset + img2.shape[1]] = img2
+
+
+    return ret
+
 def get_thermal(image, x, y, gain = True):
     """
     if gain = True then temperture range is between -40 °C to 150 °C
